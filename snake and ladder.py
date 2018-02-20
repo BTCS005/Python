f6184f3bcf16f5923dc04e4bfdcafbd26e0a1a79
Anitha Rathnam.KV <35531104+BTCS005@users.noreply.github.com>
2018-02-20 08:35:25 +0530

    Add files via upload

---
+++ snake and ladder.py
@@ -0,0 +1,14 @@
+#Snake and ladder program
+import random
+r=random.randint(1,6)
+print(r)
+count=int(input("enter the values"))
+count=count+r
+if count==8:
+    count=37
+ if count==11:
+     count=2
+ if count==100:
+     print("won")
+else:
+    print("continue playing")
