df898ffb982f664f22a4c26948018fb6c77bfdfa
Anitha Rathnam.KV <35531104+BTCS005@users.noreply.github.com>
2018-02-24 10:27:26 +0530

    Add files via upload

---
+++ Program_function.py
@@ -0,0 +1,12 @@
+#Demonstrating function
+def add(a,b):
+    #print(a+b)
+    return a+b
+    print("my anser is 5")
+def mul(a,b):
+    return a*b
+    
+    
+x=9
+y=6
+print("addition of",x,y,add(x,y),"mul of",x,y,mul(x,y))
