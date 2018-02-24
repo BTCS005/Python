68a98bc8571834a571c131f347f05355028e13ba
Anitha Rathnam.KV <35531104+BTCS005@users.noreply.github.com>
2018-02-24 10:26:04 +0530

    Add files via upload

---
+++ Program_Calculator.py
@@ -0,0 +1,15 @@
+#Program_Calulator using funtion
+x=int(input("enter the x value"))
+y=int(input("enter the y value"))
+def add(x,y):
+    return x+y
+def sub(x,y):
+    return x-y
+def div(x,y):
+    return x/y
+def mul(x,y):
+    return x*y
+print(add(x,y))
+print(sub(x,y))
+print(div(x,y))
+print(mul(x,y))
